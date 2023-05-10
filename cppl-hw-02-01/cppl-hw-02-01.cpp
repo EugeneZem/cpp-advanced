e3740844cf90c5cc60e0a3d130b233407cb98675
Eugene Zemlyanko <slayer0007@mail.ru>
2023-05-11 01:13:38 +0300

    cpp_advanced 2

---
+++ cppl-hw-02-01/cppl-hw-02-01.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+struct point 
+{
+private:
+	int m_x;
+	int m_y;
+
+public:
+	point(int x, int y) 
+	{
+		m_x = x;
+		m_y = y;
+	}
+	void print_point() 
+	{
+		std::cout << "x:" << m_x << ", y: " << m_y << std::endl;
+	}
+};
+
+
+
+int main()
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		point my_point(i, 2 * i);
+		my_point.print_point();
+	}
+	return 0;
+}
