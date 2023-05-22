0572127a3ddd603f3e3d87d9e5c207d411589861
Eugene Zemlyanko <slayer0007@mail.ru>
2023-05-22 07:24:06 +0300

    edit cpp_advanced 6

---
+++ cppl-hw-06-01/cppl-hw-06-01.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	std::string text;
+	std::cout << "[IN]: ";
+	getline(std::cin, text);
+
+	int i = 0;
+	char let = text[i];
+	++i;
+	std::map<char, int> count;
+	count.emplace(let, 1);
+	
+
+	while (text[i] != '\0')
+	{
+		let = text[i];
+		auto it = count.find(let);
+		if (it != count.end()) // В этом месте проверка типа больше / меньше не работает!!!
+		{
+			++(it->second);
+		}
+		else
+		{
+			count.emplace(let, 1);
+		}
+		++i;
+	}
+
+	using data = std::pair<char, int>;
+	std::vector<data> result;
+
+	for (const auto& i : count)
+	{
+		result.push_back(i);
+	}
+
+	std::sort(begin(result), end(result), [](const data& a, const data& b) { return a.second > b.second; });
+
+	for (const auto& i : result)
+	{
+		std::cout << i.first << " " << i.second << std::endl;
+	}
+}
---
+++ cppl-hw-06-02/cppl-hw-06-02.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::cout << "[IN]" << std::endl;
+    int num;
+    std::cin >> num;
+    std::set<int> data;
+    int d;
+    for (int i = 0; i < num; ++i)
+    {
+        std::cin >> d;
+        data.insert(d);
+    }
+
+    std::vector<int> result;
+    for (const auto& i : data)  // почему здесь const?
+    {
+        result.push_back(i);
+    }
+
+    std::sort(begin(result), end(result), [](const int& a, const int& b) { return a > b; }); // почему здесь const?
+
+    std::cout << "[OUT]" << std::endl;
+    for (const auto& i : result) // почему здесь const?
+    {
+        std::cout << i << std::endl;
+    }
+}
