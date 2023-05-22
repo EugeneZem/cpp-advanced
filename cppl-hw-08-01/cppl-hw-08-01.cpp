c93f8344f32643dd9dd82cded16e95cfa51ce096
Eugene Zemlyanko <slayer0007@mail.ru>
2023-05-22 23:45:10 +0300

    edit cpp_advanced 8

---
+++ cppl-hw-08-01/cppl-hw-08-01.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> dublicatClear(std::vector<int>);
+void print(std::string, std::vector<int>);
+
+int main()
+{
+    std::vector<int> vec { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::vector<int>::iterator it = vec.begin();
+    print("[IN]: ", vec);
+    vec = dublicatClear(vec);
+    print("[OUT]: ", vec);
+}
+
+std::vector<int> dublicatClear(std::vector<int> vec)
+{
+    std::sort(vec.begin(), vec.end());
+    std::vector<int>::iterator it = std::unique(vec.begin(), vec.end());
+    vec.erase(it, vec.end());
+    return vec;
+}
+
+void print(std::string str, std::vector<int> vec)
+{
+    std::vector<int>::iterator it = vec.begin();
+    std::cout << str;
+    while (it != vec.end())
+    {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+}
---
+++ cppl-hw-08-02/cppl-hw-08-02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template <typename T> void print_container(T);
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
+
+template <typename T> void print_container(T cont)
+{
+    auto it = cont.begin();
+    while (it != cont.end())
+    {
+        std::cout << *it << " ";
+        ++it;
+    }
+    std::cout << std::endl;
+}
