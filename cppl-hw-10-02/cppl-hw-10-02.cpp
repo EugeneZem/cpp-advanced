146b6920f0584ad4c0a26bf4eb183b3aa96ddf29
Eugene Zemlyanko <slayer0007@mail.ru>
2023-05-28 23:20:26 +0300

    add cpp_advanced 10

---
+++ cppl-hw-10-02/cppl-hw-10-02.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class node
+{
+public:
+    int m_value;
+    std::shared_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+};
+
+int main()
+{
+    {
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+        std::cout << node1->m_value << ", " << node2->m_value << std::endl;
+
+        node1 = node2;
+        std::cout << node1->m_value <<  ", " << node2->m_value << std::endl;
+
+        node2 = node1;
+        std::cout << node1->m_value << ", " << node2->m_value << std::endl;
+    }
+
+    return 0;
+}
